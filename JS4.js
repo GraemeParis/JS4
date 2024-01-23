e586152fb06ae1e43bb080a84dbd3d1879cb6d73
Graeme Paris <gparis1986@gmail.comm>
2024-01-23 16:54:22 +0000

    to-do starter code - issue with return

--- JS4.js
+++ JS4.js
@@ -36,24 +36,47 @@
 //events
 // These work by listening for a type of event - click, mouseOver, my-event-type. You pass them a "handler" or "callback" function that runs when that event is fired.
 
-{
-  /* <script>
-  const rootRef = document.getElementById("root");
-  rootRef.innerHTML = "Hello";
+// {
+//   /* <script>
+//   const rootRef = document.getElementById("root");
+//   rootRef.innerHTML = "Hello";
 
-  const input1Ref = document.getElementById("input1");
+//   const input1Ref = document.getElementById("input1");
 
-  input1Ref.addEventListener("input", (event) => {
-    // console.log(event.target.value); // This logs all the text added via the input
-    rootRef.innerHTML = event.target.value; // This allows you to add text to the pag via the dom as you type with the Div styling in this instance.
-  });
+//   input1Ref.addEventListener("input", (event) => {
+//     // console.log(event.target.value); // This logs all the text added via the input
+//     rootRef.innerHTML = event.target.value; // This allows you to add text to the pag via the dom as you type with the Div styling in this instance.
+//   });
 
-  const btn1Ref = document.getElementById("btn1");
+//   const btn1Ref = document.getElementById("btn1");
 
-  btn1Ref.addEventListener("click", (event) => {
-    console.log("Btn1 clicked", event, event.target.id); // This allows you to show which button is clicked and also low level detail on the event click istelf
-  });
+//   btn1Ref.addEventListener("click", (event) => {
+//     console.log("Btn1 clicked", event, event.target.id); // This allows you to show which button is clicked and also low level detail on the event click istelf
+//   });
 
-  console.log(btn1Ref);
-</script> */
-}
+//   console.log(btn1Ref);
+// </script> */
+// }
+
+// <script>
+//   const addRef = document.getElementById("add");
+//   const minusRef = document.getElementById("minus");
+//   const outputRef = document.getElementById("output");
+
+//   addRef.addEventListener("click", (event) => {
+//     if (outputRef.innerHTML == 10) {
+//       // This part is a defensive check to prevent you from going above 10 and provides an alert message when you try to.
+//       alert("You are greedy!");
+//       return;
+//     }
+//     outputRef.innerHTML = Number(outputRef.innerHTML) + 1;
+//   });
+
+//   minusRef.addEventListener("click", (event) => {
+//     if (outputRef.innerHTML == 0) {
+//       // this is the same for stopping you from going to below 0.
+//       return;
+//     }
+//     outputRef.innerHTML = Number(outputRef.innerHTML) - 1;
+//   });
+// </script>
